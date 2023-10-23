cc0721481f6bec894838003d696a959308e250ec
Anton <malaantt@icloud.com>
2023-10-23 22:27:37 +0300

    add IfElse project

---
+++ IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Привет, Машенька, родненькая");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}

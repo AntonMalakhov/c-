449242dac60f03ac2b55d9dd86159f58f4f327c3
Anton <malaantt@icloud.com>
2023-10-23 22:01:08 +0300

    add summ random

--- Summ/Program.cs
+++ Summ/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 982;
-int NumberB = 567;
+﻿int NumberA = new Random().Next(1, 10);
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1, 10);
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine(result);
